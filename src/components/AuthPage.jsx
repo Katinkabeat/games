cf4a2c17f584ee966e631db54bfd6e5d0d200496
Katinkabeat <tracey8008@hotmail.com>
2026-05-28 19:15:29 -0300

    Fix captcha slot to a fixed height so the auth card never reflows

--- src/components/AuthPage.jsx
+++ src/components/AuthPage.jsx
@@ -335,7 +335,7 @@ export default function AuthPage({ isRecovery = false, onPasswordReset = () => {
           </div>
 
           {TURNSTILE_SITE_KEY && (
-            <div className="flex justify-center items-center min-h-[65px]">
+            <div className="flex justify-center items-center h-[80px]">
               <Turnstile
                 ref={captchaRef}
                 siteKey={TURNSTILE_SITE_KEY}
